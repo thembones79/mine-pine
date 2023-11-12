e846363453ebb153174c8e2ed09dd5ee8bc8dac0
thembones79 <michal.szumnarski@gmail.con>
2023-11-13 00:05:56 +0100

    lua-line

--- lua/lualine/themes/mine-pine.lua
+++ lua/lualine/themes/mine-pine.lua
@@ -2,32 +2,32 @@ local p = require('mine-pine.palette')
 
 return {
 	normal = {
-		a = { bg = p.rose, fg = p.base, gui = 'bold' },
+		a = { bg = p.rose, fg = p.base},
 		b = { bg = p.overlay, fg = p.rose },
 		c = { bg = p.base, fg = p.text },
 	},
 	insert = {
-		a = { bg = p.foam, fg = p.base, gui = 'bold' },
+		a = { bg = p.foam, fg = p.base},
 		b = { bg = p.overlay, fg = p.foam },
 		c = { bg = p.base, fg = p.text },
 	},
 	visual = {
-		a = { bg = p.iris, fg = p.base, gui = 'bold' },
+		a = { bg = p.iris, fg = p.base},
 		b = { bg = p.overlay, fg = p.iris },
 		c = { bg = p.base, fg = p.text },
 	},
 	replace = {
-		a = { bg = p.pine, fg = p.base, gui = 'bold' },
+		a = { bg = p.pine, fg = p.base},
 		b = { bg = p.overlay, fg = p.pine },
 		c = { bg = p.base, fg = p.text },
 	},
 	command = {
-		a = { bg = p.love, fg = p.base, gui = 'bold' },
+		a = { bg = p.love, fg = p.base},
 		b = { bg = p.overlay, fg = p.love },
 		c = { bg = p.base, fg = p.text },
 	},
 	inactive = {
-		a = { bg = p.base, fg = p.muted, gui = 'bold' },
+		a = { bg = p.base, fg = p.muted},
 		b = { bg = p.base, fg = p.muted },
 		c = { bg = p.base, fg = p.muted },
 	},
