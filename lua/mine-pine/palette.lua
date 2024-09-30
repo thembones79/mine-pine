c3e009fb85d70fe7e8e01b6d8a2bf2d7302c4d99
thembones79 <michal.szumnarski@gmail.com>
2024-09-30 10:44:32 +0200

    updated pallette

--- lua/mine-pine/palette.lua
+++ lua/mine-pine/palette.lua
@@ -64,6 +64,26 @@ local variants = {
 		highlight_high = '#3c3531',
 		none = 'NONE',
 	},
+	tmux = {
+		_nc = "#16141f",
+		base = "#191724",
+		surface = "#1f1d2e",
+		overlay = "#26233a",
+		muted = "#6e6a86",
+		subtle = "#908caa",
+		text = "#e0def4",
+		love = "#eb6f92",
+		gold = "#f6c177",
+		rose = "#ebbcba",
+		pine = "#31748f",
+		foam = "#9ccfd8",
+		iris = "#c4a7e7",
+		leaf = "#95b1ac",
+		highlight_low = "#21202e",
+		highlight_med = "#403d52",
+		highlight_high = "#524f67",
+		none = "NONE",
+	},
 }
 
 if variants[options.variant] ~= nil then
